a935ddf84649e8c38125d56e7eb47be4129db97f
Luke McDonald <lumc8100@colorado.edu>
2020-01-29 14:19:00 -0700

    Base code

---
+++ lab2/lab2_base.ino
@@ -0,0 +1,59 @@
+#include <sparki.h>
+
+#define CYCLE_TIME .100  // seconds
+
+// Program States
+#define CONTROLLER_FOLLOW_LINE 1
+#define CONTROLLER_DISTANCE_MEASURE 2
+
+
+int current_state = CONTROLLER_FOLLOW_LINE; // Change this variable to determine which controller to run
+const int threshold = 700;
+int line_left = 1000;
+int line_center = 1000;
+int line_right = 1000;
+
+float pose_x = 0., pose_y = 0., pose_theta = 0.;
+
+void setup() {
+  pose_x = 0.;
+  pose_y = 0.;
+  pose_theta = 0.;
+}
+
+void readSensors() {
+  line_left = sparki.lineLeft();
+  line_right = sparki.lineRight();
+  line_center = sparki.lineCenter();
+  // distance = sparki.ping();
+}
+
+void measure_30cm_speed() {
+  // TODO
+}
+
+
+void updateOdometry() {
+  // TODO
+}
+
+void displayOdometry() {
+  // TODO
+}
+
+void loop() {
+
+  // TODO: Insert loop timing/initialization code here
+  
+  switch (current_state) {
+    case CONTROLLER_FOLLOW_LINE:
+      // TODO
+      break;
+    case CONTROLLER_DISTANCE_MEASURE:
+      measure_30cm_speed();
+      break;
+  }
+
+
+  delay(1000*CYCLE_TIME);
+}
